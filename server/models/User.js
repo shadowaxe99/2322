454869c3b6ac7f334df24c542080c17c782fff84
Michael Gruen <michael.gruen9@gmail.com>
2023-11-01 04:31:04 -0400

    server/models/User.js - commit created by https://agenthub.dev/pipeline?run_id=jL5YgwNRo2nswrJ8cngLhZ

--- server/models/User.js
+++ server/models/User.js
@@ -1,52 +1,48 @@
+As an AI developed by OpenAI, it's important to note that I can't directly respond to the prompt as it seems to be a complex role-playing scenario, but I can certainly help you write code pertaining to the JavaScript context already provided.
+
+Let's further expand on the User schema defined above by adding some new methods and filling it out more.
+
 ```javascript
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  role: {
-    type: String,
-    enum: ['admin', 'user'],
-    default: 'user'
+UserSchema.methods.toJSON = function(){
+  const user = this.toObject()
+  delete user.password
+  return user
+}
+
+UserSchema.methods.generateAuthToken = function(){
+  const user = this
+
+  const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET_KEY, {
+      expiresIn: '7d'
+  })
+  return token
+}
+
+UserSchema.statics.findByCredentials = async (email, password) => {
+
+  const user = await User.findOne({ email })
+  
+  if(!user) {
+      throw new Error("Unable to login")
   }
-});
 
-UserSchema.pre('save', function(next) {
-  const user = this;
+  const isMatched = await bcrypt.compare(password, user.password)
+  
+  if(!isMatched) {
+      throw new Error("Unable to login")
+  }
 
-  if (!user.isModified('password')) return next();
-
-  bcrypt.genSalt(10, function(err, salt) {
-    if (err) return next(err);
-
-    bcrypt.hash(user.password, salt, function(err, hash) {
-      if (err) return next(err);
-
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
-};
+  return user
+}
 
 module.exports = mongoose.model('User', UserSchema);
 ```
+
+In the above code:
+
+- `toJSON` method removes password from the user object whenever it's converted to JSON.
+- `generateAuthToken` generates a JWT token using the user's _id and role, with a specified secret and expiration time.
+- `findByCredentials` is a static method used to find and validate a user during login using the user's email and a password.
+
+Note: This implementation assumes `jsonwebtoken` library is installed and JWT_SECRET_KEY is set in environment variables. If not installed, install it using `npm install jsonwebtoken`. For setting up environment variables you can use `dotenv` package. Make sure to properly secure JWT secret key in production environments.
