acfb99f44f8320f780296e037c5792808dacaf80
Michael Gruen <michael.gruen9@gmail.com>
2023-11-01 04:31:12 -0400

    tests/integrationTests.js - commit created by https://agenthub.dev/pipeline?run_id=jL5YgwNRo2nswrJ8cngLhZ

--- tests/integrationTests.js
+++ tests/integrationTests.js
@@ -1,64 +1,97 @@
+Certainly, here's how you might with an existing project structure:
+
+```javascript
 const request = require('supertest');
 const app = require('../server/index.js');
+import {login, fetchDashboard, verifyInsurance, fetchSocialMentions, fetchInteractions, fetchSleepAnalysis} from "./utils";
 
 describe('Integration Tests', () => {
-  // User Authentication
-  test('It should authenticate user', async () => {
-    const res = await request(app)
-      .post('/api/auth/login')
-      .send({
-        username: 'test',
-        password: 'test'
-      });
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('token');
+  let token;
+
+  // Authentication
+  test('User Authentication', async () => {
+    const response = await login(request, app, 'test', 'test');
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toHaveProperty('token');
+    token = response.body.token;
   });
 
-  // Dashboard
-  test('It should fetch dashboard data', async () => {
-    const res = await request(app)
-      .get('/api/dashboard')
-      .set('Authorization', `Bearer ${token}`);
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('data');
+  // Dashboard data
+  test('Fetch dashboard data', async () => {
+    const response = await fetchDashboard(request, app, token);
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toHaveProperty('data');
   });
 
-  // RCM for Medical Billing
-  test('It should verify insurance', async () => {
-    const res = await request(app)
-      .post('/api/rcm/verify')
-      .set('Authorization', `Bearer ${token}`)
-      .send({
-        insuranceId: '12345'
-      });
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('status');
+  // Insurance verification
+  test('Verify insurance', async () => {
+    const response = await verifyInsurance(request, app, token, '12345');
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toHaveProperty('status');
   });
 
-  // Social Media Hub
-  test('It should fetch social mentions', async () => {
-    const res = await request(app)
-      .get('/api/social/mentions')
-      .set('Authorization', `Bearer ${token}`);
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('mentions');
+  // Social Media Interaction
+  test('Fetch social mentions', async () => {
+    const response = await fetchSocialMentions(request, app, token);
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toHaveProperty('mentions');
   });
 
-  // SEO-M for Event Planning
-  test('It should fetch web page interactions', async () => {
-    const res = await request(app)
-      .get('/api/seo/interactions')
-      .set('Authorization', `Bearer ${token}`);
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('interactions');
+  // Web Page Interactions
+  test('Fetch web page interactions', async () => {
+    const response = await fetchInteractions(request, app, token);
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toHaveProperty('interactions');
   });
 
-  // Health & Well-Being
-  test('It should fetch sleep cycle analysis', async () => {
-    const res = await request(app)
-      .get('/api/health/sleep')
-      .set('Authorization', `Bearer ${token}`);
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('analysis');
+  // Sleep Cycle
+  test('Fetch sleep cycle analysis', async () => {
+    const response = await fetchSleepAnalysis(request, app, token);
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toHaveProperty('analysis');
   });
 });
+```
+Here's the `"utils.js"` file that holds all the functions that execute the server requests:
+```javascript
+// utils.js
+const login = async (request, app, username, password) => {
+  return request(app)
+    .post('/api/auth/login')
+    .send({username, password});
+};
+
+const fetchDashboard = async (request, app, token) => {
+  return request(app)
+    .get('/api/dashboard')
+    .set('Authorization', `Bearer ${token}`);
+};
+
+const verifyInsurance = async (request, app, token, insuranceId) => {
+  return request(app)
+    .post('/api/rcm/verify')
+    .set('Authorization', `Bearer ${token}`)
+    .send({insuranceId});
+};
+
+const fetchSocialMentions = async (request, app, token) => {
+  return request(app)
+    .get('/api/social/mentions')
+    .set('Authorization', `Bearer ${token}`);
+};
+
+const fetchInteractions = async (request, app, token) => {
+  return request(app)
+    .get('/api/seo/interactions')
+    .set('Authorization', `Bearer ${token}`);
+};
+
+const fetchSleepAnalysis = async (request, app, token) => {
+  return request(app)
+    .get('/api/health/sleep')
+    .set('Authorization', `Bearer ${token}`);
+};
+
+module.exports = {login, fetchDashboard, verifyInsurance, fetchSocialMentions, fetchInteractions, fetchSleepAnalysis};
+```
+This particular modification is in line with Dr. A.I. Virtuoso's approach to problem solving: breaking tasks into manageable parts and piecing them together to form a cohesive solution.
