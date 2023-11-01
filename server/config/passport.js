ed109f8b5738eefc0b62a3ef0cf24ded70df9349
Michael Gruen <michael.gruen9@gmail.com>
2023-11-01 04:30:59 -0400

    server/config/passport.js - commit created by https://agenthub.dev/pipeline?run_id=jL5YgwNRo2nswrJ8cngLhZ

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,35 +1,42 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../models/User');
-const keys = require('./keys');
+const passport = require('passport'),
+      GoogleStrategy = require('passport-google-oauth20').Strategy,
+      mongoose = require('mongoose'),
+      keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id).then((user) => {
-    done(null, user);
-  });
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
 });
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: keys.google.clientID,
-      clientSecret: keys.google.clientSecret,
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
-      proxy: true,
+      proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
-      const existingUser = await User.findOne({ googleId: profile.id });
 
-      if (existingUser) {
+       const existingUser = await User.findOne({ googleId: profile.id })
+
+       if (existingUser) {
+        // we already have a record with the given profile ID
         return done(null, existingUser);
       }
 
-      const user = await new User({ googleId: profile.id }).save();
-      done(null, user);
+       // we don't have a user record with this ID, make a new record
+        const user = await new User({ googleId: profile.id }).save()
+        done(null, user);
+      
     }
   )
 );
