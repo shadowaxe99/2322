46a5e0b7cbbcc0e4c3e72407b8bcfd6e8bf785cf
Michael Gruen <michael.gruen9@gmail.com>
2023-11-01 04:31:07 -0400

    server/routes/authRoutes.js - commit created by https://agenthub.dev/pipeline?run_id=jL5YgwNRo2nswrJ8cngLhZ

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,16 +1,51 @@
+Given the context provided, you seem to be asking for a working solution to whatever system described, written in JavaScript involving routing with Express.js and authentication with Passport.js.
+
+It's not clear from your context what the next exact problem you want to solve is, but I will write an extended example of a registration system that uses a token-based approach.
+
 ```javascript
-const express = require('express');
-const router = express.Router();
-const passport = require('passport');
-const authController = require('../controllers/authController');
+// Additional Requirements
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-router.post('/register', authController.register);
+// Register a new User
+authController.register = async (req, res) => {
+    let user = new User(req.body);
+    user = await user.save();
 
-router.post('/login', passport.authenticate('local', { session: false }), authController.login);
+    if(!user) return res.status(404).send({ 'message': 'Error registering user' });
 
-router.post('/logout', authController.logout);
+    res.status(200).send({
+        'message': 'User registered successfully', 
+        'user': user
+    });
+}
 
-router.get('/user', passport.authenticate('jwt', { session: false }), authController.getUser);
+// Login User and return JWT
+authController.login = async (req, res) => {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
 
-module.exports = router;
+    if(!user) return res.status(404).send({ 'message': 'Error logging user' });
+
+    const token = await user.generateAuthToken();
+    res.status(200).send({user, token});
+}
+
+// Logout User by delete its token
+authController.logout = async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    user.tokens = user.tokens.filter((token) => {
+        return token.token !== req.token;
+    });
+    await user.save();
+
+    res.status(200).send({ 'message': 'Logged out' });
+}
+
+// Get User information
+authController.getUser = async (req, res) => {
+    res.status(200).send(req.user);
+}
 ```
+
+Remember, the User model must have methods for handling auth details which have not been explained in this context. This is a rudimentary example of a token-based authentication system and has room for optimization and additional features like token expiration and password hashing.
